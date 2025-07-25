883db2d5cd35681faac165c8409dc7642cae7856
samhitha bhogadi <166645416+samhithabhogadi@users.noreply.github.com>
2025-07-25 21:59:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,8 +6,9 @@ from datetime import datetime
 import yfinance as yf
 
 # Initialize theme toggle
-if 'dark_mode' not in st.session_state:
-    st.session_state['dark_mode'] = False
+mode = st.sidebar.checkbox("🌗 Dark Mode", value=st.session_state['dark_mode'])
+st.session_state['dark_mode'] = mode
+
 
 # Theme colors
 if st.session_state['dark_mode']:
