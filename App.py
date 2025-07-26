2ef87fe57bee663206ff36866e4cbc73d90e83ac
samhitha bhogadi <166645416+samhithabhogadi@users.noreply.github.com>
2025-07-26 20:46:16 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,21 +8,6 @@ import os
 # Initialize dark mode in session state
 if 'dark_mode' not in st.session_state:
     st.session_state['dark_mode'] = False
-
-# Toggle switch in sidebar
-mode = st.sidebar.checkbox("🌗 Dark Mode", value=st.session_state['dark_mode'])
-st.session_state['dark_mode'] = mode
-
-# Theme colors
-if st.session_state['dark_mode']:
-    bg_color = "#1e1e1e"  # Black background
-    text_color = "#ffffff"  # White text
-    sidebar_bg = "#2e2e2e"  # Darker sidebar
-else:
-    bg_color = "#ffffff"  # White background
-    text_color = "#000000"  # Black text
-    sidebar_bg = "#f0f2f6"  # Light sidebar
-
  #
 st.markdown("""
     <style>
